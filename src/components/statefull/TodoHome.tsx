e22bb7e7c7824d70f2bb5fa517915074716e3bce
Narendra Singh Rathore <rathoreofficialemail@gmail.com>
2019-10-29 19:21:33 +0530

    travis integration

--- src/components/statefull/TodoHome.tsx
+++ src/components/statefull/TodoHome.tsx
@@ -11,11 +11,15 @@ export class TodoHome extends React.Component<{}, ITodoHome> {
       list: [
         {
           id: "b77d409a-10cd-4a47-8e94-b0cd0ab50aa1",
-          name: "Item 1"
+          name: "Code-Splitting",
+          content:
+            "Most React apps will have their files “bundled” using tools like Webpack, Rollup or Browserify. Bundling is the process of following imported files and merging them into a single file: a “bundle”. This bundle can then be included on a webpage to load an entire app at once."
         },
         {
           id: "b77d409a-10cd-4a47-8e94-b0cd0ab50aa2",
-          name: "Item 2"
+          name: "Context",
+          content:
+            "In a typical React application, data is passed top-down (parent to child) via props, but this can be cumbersome for certain types of props (e.g. locale preference, UI theme) that are required by many components within an application. Context provides a way to share values like these between components without having to explicitly pass a prop through every level of the tree."
         }
       ]
     };
